b1ffc34ca8526995e8aacee6f75205c997507810
frkam <gearboxop@gmail.com>
2022-05-09 17:46:13 +0300

    init project, added dependencies

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-indigo-500 mx-8">
+      <h4 className="text-3xl font-bold underline text-red-600">LOL</h4>
+      <button className="btn">lol</button>
+    </div>
+  );
+}
+
+export default App;
